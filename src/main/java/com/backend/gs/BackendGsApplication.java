5741e99fb74c10734de6bec84c6078911e653bfd
yurilopess <costalopesyuri@gmail.com>
2025-11-19 23:59:38 -0300

    fix: fixing build errors

--- src/main/java/com/backend/gs/BackendGsApplication.java
+++ src/main/java/com/backend/gs/BackendGsApplication.java
@@ -3,7 +3,9 @@ package com.backend.gs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
+})
 public class BackendGsApplication {
 
     public static void main(String[] args) {
